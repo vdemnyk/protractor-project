89fd151929954a40c15ddc6331df8907b676115a
Nazar <Babii>
2021-04-06 15:03:23 +0300

    page example added

---
+++ pages/main.page.js
@@ -0,0 +1,31 @@
+let baseElementLocator = by.css('#homeslider');
+let loginBtnLocator = by.css('.login');
+
+
+class MainPage {
+
+    constructor() {
+    }
+
+    async open() {
+        await browser.get(browser.params.appUrl);
+    }
+
+    async navigateToLogin() {
+        await this.getLoginBtnElement().click();
+    }
+
+    async waitForPageLoaded() {
+        await browser.wait(protractor.ExpectedConditions.visibilityOf(this.getBaseElement()));
+    }
+
+    getBaseElement() {
+        return element(baseElementLocator);
+    }
+
+    getLoginBtnElement() {
+        return element(loginBtnLocator);
+    }    
+}
+
+module.exports = new MainPage();
