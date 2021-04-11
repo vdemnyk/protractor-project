1438bb597cf8539bd8128e4490d5d8236ab1c365
vdemnyk <viktoriia.demnyk@eleks.com>
2021-04-11 14:35:11 +0300

    hw9 done - v.3

--- spec.js
+++ spec.js
@@ -1,38 +1,34 @@
-describe('Successful login:', function(){
-  it('Open Sign in page', async function(){
+describe('Login functionality:', function(){
+  it('Successful login', async function(){
     await browser.get(browser.params.appUrl);
     await element(by.css('.login')).click();
     await browser.wait(protractor.ExpectedConditions.urlContains('authentication'));
-  });
 
-  it('Set correct credentials', async function(){
     await element(by.css('#email')).sendKeys(browser.params.email);
     await element(by.css('#passwd')).sendKeys(browser.params.password);
-  });
 
-  it('Pass authentication and sign out', async function(){
     await element(by.css('#SubmitLogin')).click();
     await browser.wait(protractor.ExpectedConditions.urlContains('my-account'));
 
     await element(by.css('.logout')).click();
     await browser.wait(protractor.ExpectedConditions.urlContains('authentication'));
   });
-});
 
-describe('Unsuccessful login:', function(){
-  it('Open Sign in page', async function(){
+  it('Unsuccessful login', async function(){
     await element(by.css('.login')).click();
     await browser.wait(protractor.ExpectedConditions.urlContains('authentication'));
-  });
 
-  it('Set incorrect credentials', async function(){
     await element(by.css('#email')).sendKeys(browser.params.email);
     await element(by.css('#passwd')).sendKeys(browser.params.wrongPassword);
-  });
 
-  it('Fail authentication', async function(){
     await element(by.css('#SubmitLogin')).click();
     await browser.wait(protractor.ExpectedConditions.urlContains('authentication'));
     await element(by.css('#center_column>.alert')).isPresent();
   });
 });
+
+
+
+
+
+
